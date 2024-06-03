289a94552f9b74e7992ed54042d617a2737ff181
Marlonleon <marlonestebanleonrojas@gmail.com>
2024-06-03 16:20:59 -0500

    trein

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
+import netlify from "@astrojs/netlify";
+
+
 export default defineConfig({
   site: 'https://Marlonleon2023.github.io',
   base: '/portafolio.github.io-',
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
